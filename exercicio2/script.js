79937a7569a224608450f046f8e1018527dabc38
marllonrp <marllonrp@yahoo.com.br>
2022-08-31 14:02:21 -0300

    Atividades diárias.

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString =
+  "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+let novaString = minhaString.trim();
+
+console.log(`Quantidade de caracteres iniciais: ${minhaString.length}
+Quatidade de caracteres após a remoção de espaços: ${novaString.length}`);
+
+let replace = novaString.replace("________", "sticioso");
+
+console.log(novaString);
+console.log(replace);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,17 @@
 //Crie a const para a frase aqui
+
+const frase =
+  'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"';
+
+let newfrase = frase.replace("verde", "rosa").replace("azul", "laranja");
+
+console.log(frase);
+console.log(newfrase);
+
+let maiuscula = "mas não deixe o gato sair";
+
+novafrase = newfrase.replace(
+  "mas não deixe o gato sair",
+  maiuscula.toUpperCase()
+);
+console.log(novafrase);
