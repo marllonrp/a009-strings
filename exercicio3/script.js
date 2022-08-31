d5db8e6757c6533a72ee613f89d97a70bb3b8eae
marllonrp <marllonrp@yahoo.com.br>
2022-08-31 15:33:51 -0300

    Correções após feedeback.

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -8,6 +8,13 @@ let newfrase = frase.replace("verde", "rosa").replace("azul", "laranja");
 console.log(frase);
 console.log(newfrase);
 
+let verificar1 = newfrase.includes("verde");
+let verificar2 = newfrase.includes("laranja");
+
+console.log(
+  `A nova string contém \"verde\"? ${verificar1}\nContém \"laranja\"? ${verificar2}`
+);
+
 let maiuscula = "mas não deixe o gato sair";
 
 novafrase = newfrase.replace(
